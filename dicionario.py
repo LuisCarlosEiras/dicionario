4374c286dbba878dd725f51773a1a9fa5ebac749
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-09 11:03:56 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -76,3 +76,81 @@ def main():
 
 if __name__ == "__main__":
     main()
+import os  
+import streamlit as st  
+from pathlib import Path  
+from dotenv import load_dotenv  
+from groq import Groq  
+
+project_root = Path(__file__).resolve().parent
+load_dotenv(project_root / ".env")
+
+class GroqAPI:
+    def __init__(self, model_name: str):
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        self.model_name = model_name
+
+    def _response(self, message):
+        return self.client.chat.completions.create(
+            model=self.model_name,
+            messages=message,
+            temperature=0,
+            max_tokens=4096,
+            stream=True,
+            stop=None,
+        )
+
+    def response_stream(self, message):        
+        for chunk in self._response(message):
+            if chunk.choices[0].delta.content:
+                yield chunk.choices[0].delta.content
+
+class Message:
+    system_prompt = "Por favor, escreva todas as respostas em português do Brasil."
+
+    def __init__(self):
+        if "messages" not in st.session_state:
+            st.session_state.messages = [{"role": "system", "content": self.system_prompt}]
+
+    def add(self, role: str, content: str):
+        st.session_state.messages.append({"role": role, "content": content})
+
+    def display_chat_history(self):
+        for message in st.session_state.messages:
+            if message["role"] == "system":
+                continue
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+    def display_stream(self, generator):
+        with st.chat_message("assistant"):
+            response_text = ""
+            for response in generator:
+                response_text += response
+            st.write(response_text)
+
+class ModelSelector:
+    def __init__(self):
+        self.models = ["llama3-70b-8192","llama3-8b-8192","mixtral-8x7b-32768","gemma-7b-it"]
+
+    def select(self):
+        with st.sidebar:
+            st.sidebar.title("Groq Chat with Llama3 + α")
+            return st.selectbox("Select a model:", self.models)
+
+def main():
+    user_input = st.chat_input("Enter message to AI models...")
+    model = ModelSelector()
+    selected_model = model.select()
+
+    message = Message()
+
+    if user_input:
+        llm = GroqAPI(selected_model)
+        message.add("user", user_input)
+        message.display_chat_history()
+        response = message.display_stream(llm.response_stream(st.session_state.messages))
+        message.add("assistant", response)
+
+if __name__ == "__main__":
+    main()
